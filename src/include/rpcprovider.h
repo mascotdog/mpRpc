f7b032f178688bf20bbce1813f3bb98702c3215d
ChenJingKai <139983251@qq.com>
2025-11-18 16:10:55 +0800

    feat: enhance RpcProvider to store service and method information

--- src/include/rpcprovider.h
+++ src/include/rpcprovider.h
@@ -3,11 +3,13 @@
 
 #include "google/protobuf/service.h"
 
-#include <memory>
+#include <functional>
+#include <google/protobuf/descriptor.h>
 #include <muduo/net/EventLoop.h>
 #include <muduo/net/InetAddress.h>
 #include <muduo/net/TcpConnection.h>
 #include <muduo/net/TcpServer.h>
+#include <string>
 
 // 框架提供的专门发布rpc服务的网络对象类
 class RpcProvider {
@@ -21,6 +23,17 @@ public:
 private:
     // 组合EventLoop
     muduo::net::EventLoop m_eventLoop;
+
+    // 服务类型信息
+    struct ServiceInfo {
+        google::protobuf::Service *m_service; // 保存服务对象
+        std::unordered_map<std::string,
+                           const google::protobuf::MethodDescriptor *>
+            m_methodMap; // 保存服务方法
+    };
+
+    // 存储注册成功的服务对象和其服务方法的所有信息 
+    std::unordered_map<std::string, ServiceInfo> m_serviceMap;
 
     // 新的socket连接回调
     void onConnection(const muduo::net::TcpConnectionPtr &conn);
