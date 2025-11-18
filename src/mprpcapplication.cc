395468b43701714f3b35b28097acd32cc883d2cf
ChenJingKai <139983251@qq.com>
2025-11-18 10:56:05 +0800

    feat: implement argument parsing and configuration file handling in Init method

--- src/mprpcapplication.cc
+++ src/mprpcapplication.cc
@@ -1,7 +1,42 @@
+#include <iostream>
+#include <unistd.h>
+
 #include "mprpcapplication.h"
+
+void ShowArgsHelp() {
+    std::cout << "format: command -i <configfiel>" << std::endl;
+}
+
 MprpcApplication &MprpcApplication::GetInsantce() {
     static MprpcApplication instance;
     return instance;
 }
 
-void MprpcApplication::Init(int argc, char **argv) {}
+void MprpcApplication::Init(int argc, char **argv) {
+    if (argc < 2) {
+        ShowArgsHelp();
+        exit(EXIT_FAILURE);
+    }
+
+    int c = 0;
+    std::string config_file;
+    while ((c = getopt(argc, argv, "i:")) != -1) {
+        switch (c) {
+        case 'i':
+            config_file = optarg;
+            break;
+        case '?':
+            std::cout << "invalid args!" << std::endl;
+            exit(EXIT_FAILURE);
+            break;
+        case ':':
+            std::cout << "need <configfile>" << std::endl;
+            ShowArgsHelp();
+            exit(EXIT_FAILURE);
+        default:
+            break;
+        }
+    }
+
+    // 开始加载配置文件 rpcserver_ip rpcserver_port zookerper_ip zookeeper_port
+}
