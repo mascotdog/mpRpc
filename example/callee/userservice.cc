a53a4cfca08e729a8b6221536f4492273e9a3b3d
ChenJingKai <139983251@qq.com>
2025-11-18 09:55:14 +0800

    feat: add CMake configuration and implementation for user service

---
+++ example/callee/userservice.cc
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+
+#include "user.pb.h"
+
+/**
+ * Userservice原本是一个本地服务，提供了两个进程内的本地方法，Login和GetFriendLists
+ */
+class UserService : public fixbug::UserServiceRpc {
+public:
+    bool Login(std::string name, std::string pwd) {
+        std::cout << "doing local service: Login" << std::endl;
+        std::cout << "name:" << name << " pwd:" << pwd << std::endl;
+    }
+
+    // 重写基类的虚函数 下面这些方法都是框架直接调用的
+    /**
+     * 1. caller -> Login(LoginRequest) -> muduo -> callee
+     * 2. callee -> Login(LoginRequest) -> 下面的重写的Login方法
+     */
+    virtual void Login(::google::protobuf::RpcController *controller,
+                       const ::fixbug::LoginRequest *request,
+                       ::fixbug::LoginResponse *response,
+                       ::google::protobuf::Closure *done) {
+        // 框架给业务上报请求参数LoginRequest，应用获取相应数据做本地业务
+        std::string name = request->name();
+        std::string pwd = request->pwd();
+
+        bool login_result = Login(name, pwd); // 做本地业务
+
+        // 把响应写入 包括错误码 错误消息 返回值
+        fixbug::ResultCode *code = response->mutable_result();
+        code->set_errcode(0);
+        code->set_errmsg("");
+        response->set_success(login_result);     
+        
+        // 执行回调操作 执行响应对象数据的序列化和网络发送(由框架完成) 
+        done->Run();
+    }
+};
+
+int main() { return 0; }
