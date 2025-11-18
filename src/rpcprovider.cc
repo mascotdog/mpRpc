be8f2b5074a4e3ed325a3095f54f0df57725af01
ChenJingKai <139983251@qq.com>
2025-11-18 17:05:50 +0800

    feat: add RpcHeader message and enhance onConnection and onMessage methods for RPC communication

--- src/rpcprovider.cc
+++ src/rpcprovider.cc
@@ -1,5 +1,6 @@
 #include "rpcprovider.h"
 #include "mprpcapplication.h"
+#include "rpcheader.pb.h"
 
 /**
  * service_name -> service 描述 -> service* 记录服务对象
@@ -62,7 +63,59 @@ void RpcProvider::Run() {
     m_eventLoop.loop();
 }
 
-void RpcProvider::onConnection(const muduo::net::TcpConnectionPtr &conn) {}
+void RpcProvider::onConnection(const muduo::net::TcpConnectionPtr &conn) {
+    if (!conn->connected()) {
+        // 和rpc Client的连接断开了
+        conn->shutdown();
+    }
+}
 
+/*
+在框架内部，RpcProvider和RpcConsumer协商好之间通信的protobuf数据类型
+service_name method_name args
+定义proto的message类型，进行数据头的序列化和反序列化
+
+格式：header_size(四个字节) + header_str + args_str
+*/
+
+// 已建立连接用户的读写事件回调
+// 如果远程有一个rpc服务的调用请求，那么OnMessage方法就会响应
 void RpcProvider::onMessage(const muduo::net::TcpConnectionPtr &conn,
-                            muduo::net::Buffer *, muduo::Timestamp) {}
+                            muduo::net::Buffer *buffer, muduo::Timestamp) {
+    // 网络接收的远程rpc调用请求的字符流 包含 method_name args
+    std::string recv_buf = buffer->retrieveAllAsString();
+
+    // 从字符流中读取前四个字节的内容
+    uint32_t header_size = 0;
+    recv_buf.copy((char *)&header_size, 4, 0);
+
+    // 根据header_size读取数据头的原始字符流，反序列化数据，得到rpc请求的详细信息
+    std::string rpc_header_str = recv_buf.substr(4, header_size);
+    mprpc::RpcHeader rpcHeader;
+    std::string service_name;
+    std::string method_name;
+    uint32_t args_size;
+    if (rpcHeader.ParseFromString(rpc_header_str)) {
+        // 数据头反序列化成功
+        service_name = rpcHeader.service_name();
+        method_name = rpcHeader.method_name();
+        args_size = rpcHeader.args_size();
+
+    } else {
+        // 数据头反序列化失败
+        std::cout << "rpc_header_str:" << rpc_header_str << " parse error!"
+                  << std::endl;
+    }
+
+    // 获取rpc方法参数的字符流数据s
+    std::string args_str = recv_buf.substr(4 + header_size, args_size);
+
+    // 打印调试信息
+    std::cout << "=====================================" << std::endl;
+    std::cout << "header_size: " << header_size << std::endl;
+    std::cout << "rpc_header_str: " << rpc_header_str << std::endl;
+    std::cout << "service_name: " << service_name << std::endl;
+    std::cout << "method_name: " << method_name << std::endl;
+    std::cout << "args_str: " << args_str << std::endl;
+    std::cout << "=====================================" << std::endl;
+}
